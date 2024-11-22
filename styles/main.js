f4620e14219b1bc179d6039b4a5a6409a0b393e6
LETRYKA <helloaryka@gmail.com>
2024-11-22 12:45:30 +0800

    Portfolio d4

---
+++ styles/main.js
@@ -0,0 +1,9 @@
+function lightMode() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+ }
+
+ function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
